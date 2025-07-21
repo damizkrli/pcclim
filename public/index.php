cdf97e45627bec465d84d3c6fe2d288e31b4df2c
damizkrli <karli.damien@gmail.com>
2025-07-21 09:30:29 +0200

    fix: trust HTTPS behind proxy (reverse proxy nginx)

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
